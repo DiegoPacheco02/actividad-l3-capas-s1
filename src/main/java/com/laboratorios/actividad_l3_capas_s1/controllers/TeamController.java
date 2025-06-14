daa292d1fc816a7a33d94a9da229fadc8ca71641
diego.rosa <00007122@uca.edu.sv>
2025-06-14 08:26:44 -0600

    feature: agregar endpoint de busqueda por nombre

--- src/main/java/com/laboratorios/actividad_l3_capas_s1/controllers/TeamController.java
+++ src/main/java/com/laboratorios/actividad_l3_capas_s1/controllers/TeamController.java
@@ -1,12 +1,16 @@
 package com.laboratorios.actividad_l3_capas_s1.controllers;
 
+import com.laboratorios.actividad_l3_capas_s1.dto.QueryTeamsDto;
 import com.laboratorios.actividad_l3_capas_s1.services.TeamServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/teams")
+@RequestMapping("/api/clubs")
 public class TeamController {
 
     private final TeamServices teamServices;
@@ -14,5 +18,15 @@ public class TeamController {
     @Autowired
     public TeamController(TeamServices teamServices) {
         this.teamServices = teamServices;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<QueryTeamsDto> getTeamByName(@RequestParam String name) {
+        try {
+            return ResponseEntity.ok(teamServices.getTeamByName(name));
+        } catch (Exception e) {
+            System.out.println("Error in TeamController.getTeamByName: " + e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
--- src/main/java/com/laboratorios/actividad_l3_capas_s1/services/TeamServices.java
+++ src/main/java/com/laboratorios/actividad_l3_capas_s1/services/TeamServices.java
@@ -1,6 +1,9 @@
 package com.laboratorios.actividad_l3_capas_s1.services;
 
+import com.laboratorios.actividad_l3_capas_s1.dto.QueryTeamsDto;
+import com.laboratorios.actividad_l3_capas_s1.model.Team;
 import com.laboratorios.actividad_l3_capas_s1.repositories.TeamRepository;
+import jakarta.persistence.NoResultException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,4 +17,18 @@ public class TeamServices {
         this.teamRepository = teamRepository;
     }
 
+    public QueryTeamsDto getTeamByName(String name) {
+        Team team = teamRepository.findByName(name);
+        if (team == null) throw new NoResultException("Team not found with name: " + name);
+
+        QueryTeamsDto queryTeamsDto = new QueryTeamsDto();
+        queryTeamsDto.setId(team.getId());
+        queryTeamsDto.setName(team.getName());
+        queryTeamsDto.setCountry(team.getCountry());
+        queryTeamsDto.setCoach(team.getCoach());
+        queryTeamsDto.setTitles(team.getTitles());
+
+        return queryTeamsDto;
+
+    }
 }
