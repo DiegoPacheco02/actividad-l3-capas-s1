76bcf7cbd1e377576f72ce7e36d8a42a4a12470e
diego.rosa <00007122@uca.edu.sv>
2025-06-14 07:27:14 -0600

    feat: commit con estructura inicial de proyecto

---
+++ src/main/java/com/laboratorios/actividad_l3_capas_s1/ActividadL3CapasS1Application.java
@@ -0,0 +1,13 @@
+package com.laboratorios.actividad_l3_capas_s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadL3CapasS1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadL3CapasS1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/laboratorios/actividad_l3_capas_s1/ActividadL3CapasS1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratorios.actividad_l3_capas_s1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadL3CapasS1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
